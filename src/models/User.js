64499e8558e4b47a4ae009aaa3d7b9ee0c3c2a30
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-09-09 18:21:34 +0200

    Fix: user fields are required

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,20 @@
 const {model, Schema} = require('mongoose');
 
 module.exports = model('User', new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 }, {timestamps: true}));
