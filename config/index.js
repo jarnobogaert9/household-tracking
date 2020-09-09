121c99ad20a4618fa228cf4c507ca8a39692b87b
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-09-09 18:42:22 +0200

    Feat: make connection with Mongodb atlas

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 module.exports = {
-    PORT: process.env.PORT || 3333 
+    PORT: process.env.PORT || 3333,
+    API_BASE_URL: '/api/v1',
+    MONGODB_CONNECTION_URI: process.env.MONGODB_CONNECTION_URI 
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 require('dotenv').config();
 
-const { PORT } = require('../config');
+const { PORT, API_BASE_URL, MONGODB_CONNECTION_URI } = require('../config');
+const mongoose = require('mongoose');
 const app = express();
+
+const tasks = require('./routes/tasks');
+
+mongoose.connect(`${MONGODB_CONNECTION_URI}`, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected');
+});
+
+app.use(express.json());
+
+app.use(`${API_BASE_URL}/tasks`, tasks);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
